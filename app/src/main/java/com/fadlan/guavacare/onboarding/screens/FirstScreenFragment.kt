a2bed0bba2a0eebcdd736ceb99a0c188aabbdbc2
fadlan7 <inifadlan7@gmail.com>
2023-05-15 15:44:04 +0700

    create button for skip onboarding

--- app/src/main/java/com/fadlan/guavacare/onboarding/screens/FirstScreenFragment.kt
+++ app/src/main/java/com/fadlan/guavacare/onboarding/screens/FirstScreenFragment.kt
@@ -1,15 +1,18 @@
 package com.fadlan.guavacare.onboarding.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.fadlan.guavacare.R
 import com.fadlan.guavacare.databinding.FragmentFirstScreenBinding
 import com.fadlan.guavacare.databinding.FragmentSecondScreenBinding
+import com.google.android.material.button.MaterialButton
 
 class FirstScreenFragment : Fragment() {
 
@@ -21,14 +24,25 @@ class FirstScreenFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_first_screen, container, false)
 
         val next = view.findViewById<TextView>(R.id.next)
+        val skip = view.findViewById<MaterialButton>(R.id.skip_button)
         val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
         next.setOnClickListener {
             viewPager?.currentItem = 1
         }
 
+        skip.setOnClickListener{
+            findNavController().navigate(R.id.action_onboardingFragment_to_homeFragment)
+            onBoardingIsFinished()
+        }
         return view
     }
 
 
+    private fun onBoardingIsFinished(){
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("finished",true)
+        editor.apply()
+    }
 }
--- app/src/main/java/com/fadlan/guavacare/onboarding/screens/SecondScreenFragment.kt
+++ app/src/main/java/com/fadlan/guavacare/onboarding/screens/SecondScreenFragment.kt
@@ -1,15 +1,18 @@
 package com.fadlan.guavacare.onboarding.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.fadlan.guavacare.R
 import com.fadlan.guavacare.databinding.FragmentFirstScreenBinding
 import com.fadlan.guavacare.databinding.FragmentSecondScreenBinding
+import com.google.android.material.button.MaterialButton
 
 class SecondScreenFragment : Fragment() {
 
@@ -22,12 +25,23 @@ class SecondScreenFragment : Fragment() {
 
         val next = view.findViewById<TextView>(R.id.next2)
         val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+        val skip = view.findViewById<MaterialButton>(R.id.skip_button)
 
         next.setOnClickListener {
             viewPager?.currentItem = 2
         }
 
+        skip.setOnClickListener{
+            findNavController().navigate(R.id.action_onboardingFragment_to_homeFragment)
+            onBoardingIsFinished()
+        }
         return view
     }
 
+    private fun onBoardingIsFinished(){
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("finished",true)
+        editor.apply()
+    }
 }
